014fecc0d3bc1e71c9515d29dc62e290ac728300
Ingle <inglesejulien@hotmail.fr>
2026-07-29 18:06:45 +0200

    Move API requirements for Render deployment

--- api/inference.py
+++ api/inference.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 
 import numpy as np
+import os
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 import tensorflow as tf
 from PIL import Image
 
